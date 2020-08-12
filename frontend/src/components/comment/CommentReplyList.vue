42f253700c555aa2b426819f8f012d61a3b47cd2
rlfslf111 <rlfslf111@naver.com>
2020-08-12 14:11:35 +0900

    exception handling

--- frontend/src/components/comment/CommentReplyList.vue
+++ frontend/src/components/comment/CommentReplyList.vue
@@ -5,28 +5,28 @@
         <div class="card-header d-flex bg-white pl-0 pr-0 pb-1 pt-3">
             <!-- nickname 칸 -->
             <div class="mr-3 d-flex my-auto" style="width:15%">
-            <strong class="text-dark my-auto" style="width:8rem; text-align:left;">{{reply.nickname}}</strong>
+                <strong class="text-dark my-auto" style="width:8rem; text-align:left;">{{reply.nickname}}</strong>
             </div>
             <!-- content 칸 -->
-            <div class="ml-3 my-auto" style="width:50%">
-            <div>
-                <span class="comment d-flex">{{reply.content}}</span>
-            </div>
+            <div class="ml-3 my-auto" style="width:40%;">
+                <div style="word-break:nowrap;">
+                    <span class="comment d-flex">{{reply.content}}</span>
+                </div>
             </div>
             <!-- 날짜와 대댓버튼 -->
-            <div class="ml-auto my-auto datereply d-flex justify-content-end row" style="width:30%;">
-            <div class="d-flex justify-content-end pl-0 col-12 col-xs-12">
-                <small class="text-dark d-flex align-items-start">{{commentdate(reply.createDate)}}</small>
-            </div>
+            <div class="ml-auto my-auto datereply d-flex justify-content-end row" style="width:40%;">
+                <div class="d-flex justify-content-end pl-0 col-12 col-xs-12">
+                    <small class="text-dark d-flex align-items-start">{{commentdate(reply.createDate)}}</small>
+                </div>
             <!-- 댓글 수정 삭제 버튼 -->
-            <div class="d-flex justify-content-end pl-0 my-auto col-12 questionbtn" style="word-break:nowrap;">
-                <small v-if="nickNameCheck" @click="commentReplyModify" class="">
-                    <span v-if="isUpdate" style="color:red">취소</span>
-                    <span v-else style="color:gray;">수정</span>
-                </small>
-                <small @click="replyDelete(reply.rrid)" v-if="nickNameCheck" class="ml-2" style="color:gray">삭제</small>
-                <small class="ml-2" style="color:gray">신고</small>
-            </div>
+                <div class="d-flex justify-content-end pl-0 my-auto col-12 questionbtn" style="word-break:nowrap;">
+                    <small v-if="nickNameCheck" @click="commentReplyModify">
+                        <span v-if="isUpdate" style="color:red">취소</span>
+                        <span v-else style="color:ForestGreen;">수정</span>
+                    </small>
+                    <small @click="replyDelete(reply.rrid)" v-if="nickNameCheck" class="ml-2" style="color:Crimson">삭제</small>
+                    <small class="ml-2" style="color:darkKhaki">신고</small>
+                </div>
             </div>
         </div>
         <!-- 댓글 수정 -->
