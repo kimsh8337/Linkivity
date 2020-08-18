f791b3e4e67188d4e3f0b679d3481780d65a5981
노휘종 <gnlwhd6498@naver.com>
2020-08-18 23:48:44 +0900

    admin page ui

--- frontend/src/page/admin/Admin.vue
+++ frontend/src/page/admin/Admin.vue
@@ -5,27 +5,36 @@
     <div class="container col-md-8 mt-5">
       <b-tabs content-class="mt-3" fill>
         <!-- 포스트 관리 -->
-        <b-tab class="admin-post" title="포스트 관리" active>
+        <b-tab class="container admin-post" title="포스트 관리" active>
           <table class="table my-5">
-      <thead class="thead-light">
-        <tr>
-          <th class="admin-post-num">No</th>
-          <th class="admin-post-title">제목</th>
-          <th class="admin-post-company">업체정보</th>
-          <th class="admin-post-email">작성자</th>
-          <th class="admin-post-date">요청 날짜</th>
-          <th class="admin-post-out">승인 및 거절</th>
-        </tr>
-
+            <thead class="thead-dark">
+              <tr>
+                <th class="admin-post-num">No</th>
+                <th class="admin-post-title">제목</th>
+                <th class="admin-post-company">업체정보</th>
+                <th class="admin-post-email">작성자</th>
+                <th class="admin-post-date">요청 날짜</th>
+                <th class="admin-post-out">승인 및 거절</th>
+              </tr>
             </thead>
             <tbody>
-              <tr v-for="(post, index) in postlists" :key="index">
-                <td>{{ ((ppage - 1) * 10) + index + 1 }}</td>
-                <td class="admin-data">{{ post.title }}</td>
+              <tr id="tt" v-for="(post, index) in postlists" :key="index">
+                <td>{{ (ppage - 1) * 10 + index + 1 }}</td>
+                <td class="admin-data" style="padding:10px">{{ post.title }}</td>
                 <td class="admin-data">{{ post.companyInfo }}</td>
                 <td class="admin-data">{{ post.email }}</td>
                 <td class="admin-data">{{ permitdate(post.createDate) }}</td>
-                <td><button @click="gopostdetail(post.pid)">상세보기</button></td>
+                <td>
+                  <button
+                    @click="gopostdetail(post.pid)"
+                    type="button"
+                    class="btn btn-sm ml-0"
+                    style="color:white; background-color:RGB(134, 165, 212); border-radius:7px;"
+                  >
+                    상세보기
+                  </button>
+                </td>
+                <!-- <td><button @click="gopostdetail(post.pid)">상세보기</button></td> -->
                 <!-- <td><button @click="postlistData(post)" data-toggle="modal" data-target="#postpermit">상세보기</button></td> -->
                 <!-- <td><button @click="postpermit(post.pid)">승인</button> <button @click="postreject(post.pid)">거절</button></td> -->
               </tr>
@@ -39,25 +48,26 @@
         <!-- 회원 관리 -->
         <b-tab class="admin-info" title="회원관리">
           <table class="table my-5">
-      <thead class="thead-light">
-        <tr>
-          <th class="admin-info-num">No</th>
-          <th class="admin-info-email">이메일</th>
-          <th class="admin-info-nickname">닉네임</th>
-          <th class="admin-info-name">이름</th>
-          <th class="admin-info-type">사용자 유형</th>
-          <th class="admin-info-out">탈퇴</th>
-        </tr>
-
+            <thead class="thead-dark">
+              <tr>
+                <th class="admin-info-num">No</th>
+                <th class="admin-info-email">이메일</th>
+                <th class="admin-info-nickname">닉네임</th>
+                <th class="admin-info-name">이름</th>
+                <th class="admin-info-type">구분</th>
+                <th class="admin-info-out">탈퇴</th>
+              </tr>
             </thead>
             <tbody>
-              <tr v-for="(user, index) in users" :key="index">
-                <td>{{ ((upage - 1) * 10 ) + index + 1}}</td>
+              <tr id="tt" v-for="(user, index) in users" :key="index">
+                <td>{{ (upage - 1) * 10 + index + 1 }}</td>
                 <td class="admin-data">{{ user.email }}</td>
                 <td class="admin-data">{{ user.nickname }}</td>
                 <td class="admin-data">{{ user.name }}</td>
                 <td class="admin-data">{{ user.checkType }}</td>
-                <td v-if="user.uid != 1"><button class="admin-info-button" @click="dropuser(user.uid)">탈퇴</button></td>
+                <td class="pl-1" v-if="user.uid != 1">
+                  <button class="admin-info-button btn btn-sm btn-outline-danger" @click="dropuser(user.uid)">탈퇴처리</button>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -66,35 +76,47 @@
         </b-tab>
 
         <!-- 신고 관리 -->
-        <b-tab title="신고 관리"
-          ><p></p>
+        <b-tab title="신고 관리">
           <table class="table my-5">
-      <thead class="thead-light">
-        <tr>
-          <th class="admin-black-num">No</th>
-          <th class="admin-black-email">신고한사람</th>
-          <th class="admin-black-remail">신고당한사람</th>
-          <th class="admin-black-content">내용</th>
-          <th class="admin-black-detail">상세보기</th>
-          <th class="admin-black-out">취소/경고/탈퇴</th>
-
+            <thead class="thead-dark">
+              <tr>
+                <th class="admin-black-num">No</th>
+                <th class="admin-black-email">신고한사람</th>
+                <th class="admin-black-remail">신고당한사람</th>
+                <th class="admin-black-content">내용</th>
+                <th class="admin-black-detail">확인</th>
+                <th class="admin-black-out">구분</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(blacklist, index) in blacklists" :key="index">
-                <td>{{ ((bpage - 1) * 10) + index + 1 }}</td>
+              <tr id="tt" v-for="(blacklist, index) in blacklists" :key="index">
+                <td>{{ (bpage - 1) * 10 + index + 1 }}</td>
                 <td class="admin-data">{{ blacklist.email }}</td>
                 <td class="admin-data">{{ blacklist.remail }}</td>
                 <td class="admin-data">{{ blacklist.reason }}</td>
-                <td class="admin-data">
-                  <button class="admin-black-view" @click="blacklistData(blacklist)" data-toggle="modal" data-target="#blacklist">
-                    상세보기
+                <td class="pl-1">
+                  <button
+                    class="admin-black-view btn-sm"
+                    style="color:white; background-color:RGB(134, 165, 212); border-radius:7px;"
+                    @click="blacklistData(blacklist)"
+                    data-toggle="modal"
+                    data-target="#blacklist"
+                  >
+                    확인
                   </button>
                 </td>
-                <td>
-                  <button class="admin-black-warn" @click="warnblack(blacklist.rpid)">경고</button
-                  ><button class="admin-black-drop" @click="dropblack(blacklist.rpid)">탈퇴</button
-                  ><button class="admin-black-cancel" @click="cancelblack(blacklist.rpid)">취소</button>
+                <td class="pl-1">
+                  <div class="btn-group btn-group-sm">
+                    <button type="button" class="btn btn-danger">처리</button>
+                    <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
+                      <span class="caret"></span>
+                    </button>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item admin-drop-item" style="cursor:pointer" @click="warnblack(blacklist.rpid)">경고</a>
+                      <a class="dropdown-item admin-drop-item" style="cursor:pointer" @click="dropblack(blacklist.rpid)">삭제</a>
+                      <a class="dropdown-item admin-drop-item" style="cursor:pointer" @click="cancelblack(blacklist.rpid)">취소</a>
+                    </div>
+                  </div>
                 </td>
               </tr>
             </tbody>
@@ -231,14 +253,42 @@ export default {
     },
 
     dropuser(uid) {
-      axios
-        .delete(`${baseURL}/account/dropUser/${uid}`)
-        .then(() => {
-          this.userlist();
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      Swal.fire({
+        width: 350,
+        text: '탈퇴시키겠습니까?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: '<a style="font-size:1rem; color:white">탈퇴</a>',
+        cancelButtonText: '<a style="font-size:1rem; color:white">취소</a>',
+      }).then((result) => {
+        if (result.value) {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            onOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer);
+              toast.addEventListener('mouseleave', Swal.resumeTimer);
+            },
+          });
+          Toast.fire({
+            icon: 'success',
+            title: '회원 탈퇴가 완료되었습니다.',
+          });
+          axios
+            .delete(`${baseURL}/account/dropUser/${uid}`)
+            .then(() => {
+              this.userlist();
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      });
     },
     dropblack(rpid) {
       axios
@@ -313,4 +363,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.admin-drop-item:hover {
+  color: red !important;
+}
+</style>
