8dfb7910bc4398373c76f100580e07d9c77d11aa
rlfslf111 <rlfslf111@naver.com>
2020-08-19 21:20:48 +0900

    작은 수정

--- backend/src/main/java/com/web/blog/controller/post/TempListController.java
+++ backend/src/main/java/com/web/blog/controller/post/TempListController.java
@@ -213,7 +213,7 @@ public class TempListController {
         File file = null;
         if(hostname.substring(0,7).equals("DESKTOP")){
             //local
-            file = new File("C:\\leejaein\\project-sub3\\s03p13b206\\frontend\\src\\assets\\file\\" + fileName);
+            file = new File("C:\\Users\\multicampus\\Desktop\\janerun\\s03p13b206\\frontend\\public\\contents\\" + fileName);
         }else{
             //aws
             file = new File("/home/ubuntu/ssafy6/s03p13b206/frontend/public/contents/" + fileName);
